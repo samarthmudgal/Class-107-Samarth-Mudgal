6429fd2ea1928cc533a2384db9b054c84becc5d5
samarthmudgal <90464416+samarthmudgal@users.noreply.github.com>
2023-05-04 14:29:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/BtakhbH1l/model.json', modelReady);
+
+}
+
+function modelReady()
+{
+    classifier.classify(gotResults);
+}
